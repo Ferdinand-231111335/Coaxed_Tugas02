31f7a68c2ca32762ef98d1f5fbace46a3a00043a
Ferdinand Louis <ferdinand.louis@students.mikroskil.ac.id>
2025-07-06 23:50:32 +0700

    Model Makhluk untuk Provider makhluk

---
+++ lib/models/makhluk.dart
@@ -0,0 +1,19 @@
+class Makhluk {
+  final String nama;
+  final String habitat;
+  final String kategori;
+  final String deskripsi;
+  final String status;
+  final String gambar1;
+  final String gambar2;
+
+  Makhluk({
+    required this.nama,
+    required this.habitat,
+    required this.kategori,
+    required this.deskripsi,
+    required this.status,
+    required this.gambar1,
+    required this.gambar2,
+  });
+}
